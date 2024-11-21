5cc349f822a8e6cb24351af53257c6ca7e5cce88
Spandan Barve <spandan567@gmail.com>
2024-11-21 20:51:25 +0530

    initial commit

--- packages/client/src/router.tsx
+++ packages/client/src/router.tsx
@@ -4,11 +4,6 @@ import Layout from "./Layout.tsx";
 import { useLocation } from "preact-iso";
 
 import _404 from "./pages/_404.tsx";
-import Login from "./pages/login/page.tsx";
-import You from "./pages/you/page.tsx";
-import Discover from "./pages/discover/page.tsx";
-import Portfolio from "./pages/portfolio/page.tsx";
-import ManagerRegistration from "./pages/managerRegistration/page.tsx";
 
 export default function Router() {
   return (
@@ -16,16 +11,6 @@ export default function Router() {
       <ErrorBoundary onError={(e) => console.error(e)}>
         <PreactRouter>
           <Route path="" component={FallbackRoute} />
-
-          <Route path="login" component={Login} />
-
-          <Route path="mirox" component={You} />
-
-          <Route path="discover" component={Discover} />
-
-          <Route path="portfolio" component={Portfolio} />
-
-          <Route path="new-manager" component={ManagerRegistration} />
 
           <Route default component={_404} />
         </PreactRouter>
@@ -37,7 +22,7 @@ export default function Router() {
 function FallbackRoute() {
   const location = useLocation();
 
-  location.route("/discover");
+  location.route("/home");
 
   return <></>;
 }
